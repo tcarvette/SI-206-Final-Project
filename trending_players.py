faad42ef19931d4807c3f80f667157751e91985a
noahjm59 <57961136+noahjm59@users.noreply.github.com>
2020-04-21 13:29:39 -0700

    Update trending_players.py

--- trending_players.py
+++ trending_players.py
@@ -4,8 +4,9 @@ import sqlite3
 
 pytrends = TrendReq(hl="en-US", tz=360)
 
-kw_list = ["kevin durant", "james harden", "lebron james"]
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+kw_list = ["kevin durant", "james harden", "lebron james", "justise winslow", "cody zeller"]
+# ^ only 5 names at a time, will add 5 at a time to sql
+pytrends.build_payload(kw_list, cat=1077, timeframe='today 5-y', geo='', gprop='')
 a = pytrends.interest_over_time()
 for i in kw_list:
     print(i + ": " + str(a[i].mean()))
