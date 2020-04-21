778d14728795e9a93c54e7f192ed422724ef0cf0
acarvett <acarvett@umich.edu>
2020-04-21 12:25:23 -0400

    Started working on this but need balldontlie info to keep going

--- player_contract_website.py
+++ player_contract_website.py
@@ -1,1 +1,27 @@
+import re
+import json
+import sys
+import requests
+import sqlite3
+from bs4 import BeautifulSoup
+import random
 
+def get_player_contract():
+    count = 0
+    base_url = "https://www.basketball-reference.com/contracts/players.html"
+    r = requests.get(base_url)
+    soup = BeautifulSoup(r.text, "lxml")
+
+    player_list = []
+    main = soup.findAll('tbody')
+    for tr in main:
+        names = tr.findAll('a')
+        for a in names:
+            if count % 2 == 0:
+                player_list.append(a.text)
+            count += 1
+
+    return player_list
+
+
+get_player_contract()
